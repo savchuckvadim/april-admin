b2338272ed696ec8e5f423e537d295d944f0ef40
savchuckvadim <savchuckvadim@gmail.com>
2023-06-30 13:28:06 +0200

    test deploy

---
+++ src/secret/secret.js
@@ -0,0 +1,10 @@
+
+export const secretFirebase = {
+    apiKey: "AIzaSyCX-e_5LuVuDKUKnsM0y6gRTFcqHW9PFLs",
+    authDomain: "april-garant.firebaseapp.com",
+    projectId: "april-garant",
+    storageBucket: "april-garant.appspot.com",
+    messagingSenderId: "415478492885",
+    appId: "1:415478492885:web:201a14c5fc5b9d6c54bc5c",
+    measurementId: "G-6P3VP33EVV"
+  }
